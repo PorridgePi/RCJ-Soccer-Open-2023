61ebe73377bc3c3c1c8e6029d79f471f170da268
PorridgePi <kunbao2006@126.com>
2023-02-15 14:23:20 +0800

    chore: Format code

--- src/test/esp32.cpp
+++ src/test/esp32.cpp
@@ -1,16 +1,17 @@
-#include <Arduino.h>
 #include "Temt.h"
 #include "Wire.h"
-#define THRESHOLD_VOLTAGE 100
-// PIN, X POSITION, Y POSITION
-//   25   33
-//26         32   
-//27         04
-//14         02
-//   13   15
+#include <Arduino.h>
+#define TEMT_THRESHOLD 100
 
-float angle = 0;
-bool canSeeLine = false;
+// PIN, X POSITION, Y POSITION
+//    25   33
+// 26         32
+// 27         04
+// 14         02
+//    13   15
+
+float angle      = 0;
+bool  canSeeLine = false;
 
 Temt temts[10] = {
     Temt(33, 0.4, 1),
@@ -27,20 +28,20 @@ Temt temts[10] = {
 
 void request() {
     int16_t angleToBeSent = angle;
-    byte packet[2];
+    byte    packet[2];
     packet[0] = (angleToBeSent >> 8) & 0xFF;
     packet[1] = angleToBeSent & 0xFF;
     Wire.write(packet, 2);
 }
 
-
 void setup() {
-    Wire.begin(54); //Need to change address?
+    Wire.begin(54); // Need to change address?
     Serial.begin(115200);
     Wire.onRequest(request);
 }
 
 /*
+
 Wire.requestFrom(52,2);
 byte packet[2];
 packet[1] = Wire.read();
@@ -49,26 +50,27 @@ if (packet[1] == 0xFF) {
     return 256 * packet[1] + packet[2];
 } else {
     return packet[1]
-} 
+}
 
-//Need to make the part that detects the 65535 value when nothing it detected by them temts
+// Need to make the part that detects the 65535 value when nothing it detected by them temts
+
 */
 
-void loop(){
+void loop() {
     float sumX = 0;
     float sumY = 0;
     for (int i = 0; i < 11; i++) {
-        if (temts[i].read() > THRESHOLD_VOLTAGE) {
-            sumX =  sumX + temts[i].X;
+        if (temts[i].read() > TEMT_THRESHOLD) {
+            sumX = sumX + temts[i].X;
             sumY = sumY + temts[i].Y;
-        } 
+        }
     }
     if (sumX != 0 || sumY != 0) {
         canSeeLine = true;
-        angle = atan2f(sumX, sumY) / 3.14159265358979323846f * 180;
+        angle      = atan2f(sumX, sumY) / 3.14159265358979323846f * 180;
     } else {
-        angle = 65535; //Largest 16 it number, used to indicate when there is no need to avoid the line
+        angle = 65535; // Largest 16 it number, used to indicate when there is
+                       // no need to avoid the line
     }
     Serial.println(angle);
 }
-
