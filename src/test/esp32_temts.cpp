e7e1a96e1827f3932d61efa2998c7591a7f593ce
PorridgePi <kunbao2006@126.com>
2023-02-15 15:02:49 +0800

    feat: Add simple test code for ESP32 and TEMTs without library

--- src/test/esp32_temts.cpp
+++ src/test/esp32_temts.cpp
@@ -1,11 +1,35 @@
 #include "Arduino.h"
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
+// PINS FOR TEMTS
+//    25   33
+// 26         32
+// 27         04
+// 14         02
+//    13   15
+
 void loop() {
-    int reading;
-    reading = analogRead(39);
-    Serial.println(reading);
+    Serial.print(analogRead(33));
+    Serial.print("\t");
+    Serial.print(analogRead(32));
+    Serial.print("\t");
+    Serial.print(analogRead(4));
+    Serial.print("\t");
+    Serial.print(analogRead(2));
+    Serial.print("\t");
+    Serial.print(analogRead(15));
+    Serial.print("\t");
+    Serial.print(analogRead(13));
+    Serial.print("\t");
+    Serial.print(analogRead(14));
+    Serial.print("\t");
+    Serial.print(analogRead(27));
+    Serial.print("\t");
+    Serial.print(analogRead(26));
+    Serial.print("\t");
+    Serial.println(analogRead(25));
+    delay(10);
 }
