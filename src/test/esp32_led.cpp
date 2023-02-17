430edf23cb77f8d75426604e987af44c3bd20a23
PorridgePi <kunbao2006@126.com>
2023-02-17 11:48:34 +0800

    feat: Have LED code in setup only

--- src/test/esp32_led.cpp
+++ src/test/esp32_led.cpp
@@ -5,12 +5,16 @@ sk led;
 
 void setup() {
     led.begin(18, 10);
-}
-
-void loop() {
-    led.begin(18, 10);
     for (int i = 0; i < 10; i++) {
         led.color(i, 255, 255, 255, 255);
     }
     led.show();
+}
+
+void loop() {
+    // led.begin(18, 10);
+    // for (int i = 0; i < 10; i++) {
+    //     led.color(i, 255, 255, 255, 255);
+    // }
+    // led.show();
 }
