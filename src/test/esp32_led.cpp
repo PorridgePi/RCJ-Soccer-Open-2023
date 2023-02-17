8e5394a2eba308f949a47eb8a7cd3769f4e17066
PorridgePi <kunbao2006@126.com>
2023-02-17 11:47:49 +0800

    fix: Update new LED pin from 12 to 18

--- src/test/esp32_led.cpp
+++ src/test/esp32_led.cpp
@@ -4,11 +4,11 @@
 sk led;
 
 void setup() {
-    led.begin(12, 10);
+    led.begin(18, 10);
 }
 
 void loop() {
-    led.begin(12, 10);
+    led.begin(18, 10);
     for (int i = 0; i < 10; i++) {
         led.color(i, 255, 255, 255, 255);
     }
