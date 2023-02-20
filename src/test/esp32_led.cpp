3654f5a18e433bfe20fe4141a04e4958f93e0c1c
AntiSocial-Taco <103398800+AntiSocial-Taco@users.noreply.github.com>
2023-02-20 15:47:50 +0800

    Added some text code to esp32_led.cpp

--- src/test/esp32_led.cpp
+++ src/test/esp32_led.cpp
@@ -4,13 +4,19 @@
 sk led;
 
 void setup() {
-    led.begin(12, 10);
-}
-
-void loop() {
-    led.begin(12, 10);
+    led.begin(18, 10);
     for (int i = 0; i < 10; i++) {
-        led.color(i, 255, 255, 255, 255);
+        led.color(i, 100,100,100,100);
+        //led.color(i, 255*(0.5*(1+sin(0.01*millis()))), 10, 10, 10);
     }
     led.show();
+
+}
+void loop() {
+    //led.begin(18, 10);
+    /*for (int i = 0; i < 10; i++) {
+        led.color(i, 10,10,10,10);
+        //led.color(i, 255*(0.5*(1+sin(0.01*millis()))), 10, 10, 10);
+    }
+    led.show();*/
 }
