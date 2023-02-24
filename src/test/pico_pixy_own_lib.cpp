72e187a0c8c303f46cf29469235fdcfd3e8bcc1e
PorridgePi <kunbao2006@126.com>
2023-02-24 18:31:26 +0800

    chore: Delete unused code

--- src/test/pico_pixy_own_lib.cpp
+++ src/test/pico_pixy_own_lib.cpp
@@ -2,6 +2,7 @@
 #include <SoftwareSerial.h>
 
 #define DEBUG_DELAY false
+#define DEBUG_LED false
 
 #define PIXY_TX 8
 #define PIXY_RX 15
@@ -21,49 +22,8 @@ int signature, x, y, width, height;
 int headers[6];
 int buffer[100];
 
-void parseData() {
-    // bytes 6-7 = signature
-    // bytes 8-9 = x
-    // bytes 10-11 = y
-    // bytes 12-13 = width
-    // bytes 14-15 = height
-    if (true || buffer[0] != 175 || buffer[1] != 193 || buffer[2] != 33 || buffer[3] == 0 || buffer[3] % 14 != 0) {
-        return;
-    } else {
-        for (int j = 0; j < 15; j++) {
-            Serial.print(buffer[j]);
-            Serial.print("\t");
-        }
-        Serial.println();
-    }
-    int signature = buffer[6] + buffer[7] * 256;
-    int x         = buffer[8] + buffer[9] * 256;
-    int y         = buffer[10] + buffer[11] * 256;
-    int width     = buffer[12] + buffer[13] * 256;
-    int height    = buffer[14] + buffer[15] * 256;
-}
-
 void printData() {
-    // Serial.print(signature);
-    // if (true) {
-    // // if (signature == 1) {
-    //     Serial.print("\t");
-    //     Serial.print(x);
-    //     Serial.print("\t");
-    //     Serial.print(y);
-    //     Serial.print("\t");
-    //     Serial.print(width);
-    //     Serial.print("\t");
-    //     Serial.print(height);
-    //     Serial.print("\t");
-    // }
-    // if (buffer[2] == 33) {
-    // for (int j = 0; j < 20; j++) {
-    //     Serial.print(buffer[j]);
-    //     Serial.print("\t");
-    // }
-    // Serial.println();
-    // }
+
 }
 
 int i = 2;
@@ -80,12 +40,7 @@ void parseData2() {
     // bytes 14-15 = height
     
     if (buffer[2] == 33 && buffer[6] > 0) {
-        // for (int j = 0; j < 20; j++) {
-        //     Serial.print(buffer[j]);
-        //     Serial.print("\t");
-        // }
-        // Serial.println();
-        digitalWrite(PIN_LED, HIGH);
+        if (DEBUG_LED) digitalWrite(PIN_LED, HIGH);
     }
 
     // int signature = buffer[6] + buffer[7] * 256;
@@ -100,13 +55,6 @@ void PixySerialEvent() {
     while (PixySerial.available() > 0 && newData == false) {
         prevNum = r;
         r = PixySerial.read();
-
-        // Serial.print(i);
-        // Serial.print('\t');
-        // Serial.print(r);
-        // Serial.print('\t');
-        // Serial.print(prevNum);
-        // Serial.println();
 
         if (recvInProgress == true) {
             if (i == 3) {
@@ -129,65 +77,7 @@ void PixySerialEvent() {
             buffer[1] = 193;
             recvInProgress = true;
         }
-
-
-        // buffer[i] = r;
-        // i++;
-
-        // if (r != 175) {
-        //     buffer[i] = r;
-        //     i++;
-        // } else {
-        //     int nextR = PixySerial.read();
-        //     if (r != 193) {
-        //         buffer[i] = r;
-        //         buffer[i + 1] = nextR;
-        //         i += 2;
-        //     } else {
-        //         buffer[0] = 175;
-        //         buffer[1] = 193;
-        //         i = 2;
-        //         newData = true;
-        //     }
-        // }
     }
-    // if (PixySerial.available()) {
-    //     headers[0] = PixySerial.read();
-    // }
-    // while (headers[0] != 175) { // 16-bit sync - Bit 0 = 175
-    //     if (PixySerial.available()) {
-    //         headers[0] = PixySerial.read();
-    //     }
-    // }
-    // for (int i = 1; i < 6; i++) {
-    //     if (PixySerial.available()) {
-    //         headers[i] = PixySerial.read();
-    //     }
-    // }
-
-    // int packetType    = headers[2]; // should be 33
-    // int payloadLength = headers[3]; // multiple of 14
-
-    // if (headers[1] == 193 && packetType == 33 && payloadLength > 0) { // 16-bit sync - Bit 1 = 193
-
-    //     for (int i = 0; i < 6; i++) {
-    //         Serial.print(headers[i]);
-    //         Serial.print("\t");
-    //     }
-
-    //     for (int i = 0; i < payloadLength + 1; i++) { // +1 to re-run loop
-    //         if (i % 14 == 0) {
-    //             if (i > 0) {
-    //                 newData = true;
-    //             }
-    //         }
-    //         if (i != payloadLength + 1) {
-    //             if (PixySerial.available()) {
-    //                 block[i % 14] = PixySerial.read();
-    //             }
-    //         }
-    //     }
-    // }
 }
 
 void loop() {
@@ -199,7 +89,6 @@ void loop() {
 
     if (newData == true) {
         parseData2();
-        // parseData();
         // printData();
         memset(buffer, 0, sizeof(buffer));
         newData = false;
@@ -212,5 +101,5 @@ void loop() {
     // Serial.print("\t");
     Serial.print((float)(micros()-time)/1000);
     Serial.println("ms");
-    digitalWrite(PIN_LED, LOW);
+    if (DEBUG_LED) digitalWrite(PIN_LED, LOW);
 }
