42f52b1a65eda9b3708c7c805a61d5be3d49aee1
PorridgePi <kunbao2006@126.com>
2023-03-03 18:26:14 +0800

    feat: Add Camera to main.cpp and debug prints

--- lib/private/Definitions.h
+++ lib/private/Definitions.h
@@ -1,7 +1,11 @@
+#define USE_MULTICORE true // for fast SoftwareSerial
+
 #define I2C_ADDRESS_ESP32 8
 
 // Thresholds
-#define LIGHT_GATE_THRESHOLD 400
+#define EMPTY_LIGHT_GATE_THRESHOLD 200
 
 // Pins
 #define LIGHT_GATE_PIN A2
+#define PIXY_TX 8
+#define PIXY_RX 15
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
 #include <Definitions.h>
+#include <Camera.h>
 
 #define DEBUG true
+#define DEBUG_PRINT_LIGHT_GATE false
+#define DEBUG_PRINT_PIXY false
+#define DEBUG_LOOP_TIME false
+
+Camera Pixy(PIXY_RX, PIXY_TX, 142, 118);
+
+unsigned long loopStartMicros;
 
 bool isBallInGate() {
-    if (analogRead(LIGHT_GATE_PIN) < LIGHT_GATE_THRESHOLD) {
+    if (analogRead(LIGHT_GATE_PIN) < EMPTY_LIGHT_GATE_THRESHOLD) {
         return true;
     } else {
         return false;
@@ -13,12 +21,31 @@ bool isBallInGate() {
 
 void setup() {
     Serial.begin(9600);
+    Pixy.begin(19200);
 }
 
 void loop() {
-    if (isBallInGate()) {
-        digitalWrite(PIN_LED, HIGH);
-    } else {
-        digitalWrite(PIN_LED, LOW);
+    loopStartMicros = micros();
+    if (!USE_MULTICORE) Pixy.readData();
+    Pixy.isNewDataPresent();
+
+    if (DEBUG_PRINT_LIGHT_GATE) {
+        Serial.print(isBallInGate()); Serial.print("\t");
+        Serial.print(analogRead(LIGHT_GATE_PIN)); Serial.print("\t");
     }
+    if (DEBUG_PRINT_PIXY) {
+        Serial.print(Pixy.getBallDistance()); Serial.print("\t");
+        Serial.print(Pixy.getBallAngle()); Serial.print("\t");
+    } 
+    if (DEBUG_LOOP_TIME) {
+        Serial.print((float) (micros() - loopStartMicros) / 1000);
+        Serial.print("\t");
+    }
+    if (DEBUG_PRINT_LIGHT_GATE || DEBUG_PRINT_PIXY || DEBUG_LOOP_TIME) Serial.println();
+    
+    if (millis() % 1000 == 0) Serial.println(millis()/1000);
+}
+
+void loop1() {
+    if (USE_MULTICORE) Pixy.readData();
 }
