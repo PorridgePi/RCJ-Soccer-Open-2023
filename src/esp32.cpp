6edadf832b1a17bace59a7232cd2fbe83de288b9
PorridgePi <kunbao2006@126.com>
2023-03-03 17:19:28 +0800

    chore: Formatting, organisation and add comments

--- src/esp32.cpp
+++ src/esp32.cpp
@@ -3,6 +3,7 @@
 #include <Temt.h>
 #include <Wire.h>
 
+#define I2C_ADDRESS_ESP32 8
 #define TEMT_THRESHOLD 1000 // to be calibrated
 #define DEBUG          true
 
@@ -30,10 +31,12 @@ Temt temts[10] = {
     Temt(25, -0.4, 1),
 };
 
+// Write the data to the I2C bus on request
 void request() {
     Wire.write(data, sizeof(data));
 }
 
+// Set the color of the LED
 void led_color(int pin, int r, int g, int b, int w) {
     led.begin(pin, 10);
     for (int i = 0; i < 10; i++) {
@@ -45,31 +48,15 @@ void led_color(int pin, int r, int g, int b, int w) {
 void setup() {
     Serial.begin(115200);
 
-    Wire.begin(8); // Need to change address?
+    Wire.begin(I2C_ADDRESS_ESP32);
     Wire.onRequest(request);
 
-    led_color(18, 255, 0, 0, 0);
+    led_color(18, 255, 0, 0, 0); // Non-white to indicate power on
     delay(500);
-    led_color(18, 0, 0, 0, 0);
+    led_color(18, 0, 0, 0, 0); // Turn off the LED temporarily to blink
     delay(500);
-    led_color(18, 255, 255, 255, 255);
+    led_color(18, 255, 255, 255, 255); // White to indicate ready
 }
-
-/*
-
-Wire.requestFrom(52,2);
-byte packet[2];
-packet[1] = Wire.read();
-packet[2] = Wire.read();
-if (packet[1] == 0xFF) {
-    return 256 * packet[1] + packet[2];
-} else {
-    return packet[1]
-}
-
-// Need to make the part that detects the 65535 value when nothing it detected by the temts
-
-*/
 
 void loop() {
     // Reset the values
@@ -77,6 +64,7 @@ void loop() {
     bool  canSeeLine = false;
     static int angle;
 
+    // Read the values from the sensors
     for (int i = 0; i < 10; i++) {
         if (DEBUG) {
             Serial.print(temts[i].read());
@@ -89,19 +77,20 @@ void loop() {
         }
     }
 
+    // Calculate the angle
     if (sumX != 0 || sumY != 0) {
         canSeeLine = true;
         angle      = atan2f(-sumX, -sumY) / 3.14159265358979323846f * 180;
-        if (angle < 0) {
-            angle += 360;
-        }
+        if (angle < 0) angle += 360; // Convert to 0-359 degrees
     } else {
-        angle = 65535; // Largest 16 bit number, used to indicate when there is no need to avoid the line
+        angle = 65535; // Largest 16 bit number indicating no need to avoid line
     }
 
+    // Send the data to the ESP32
     data[0] = angle & 0xFF;
     data[1] = (angle >> 8) & 0xFF;
 
+    // Print the data for debugging
     if (DEBUG) {
         Serial.print(sumX);
         Serial.print("\t");
