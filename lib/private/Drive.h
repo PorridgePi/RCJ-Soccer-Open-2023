e5e7f1468183dde0792ecdc19a52fddb5fb37737
PorridgePi <kunbao2006@126.com>
2023-03-08 16:51:11 +0800

    chore: Remove unused code from Drive.h

--- lib/private/Drive.h
+++ lib/private/Drive.h
@@ -4,23 +4,9 @@
 #include <Arduino.h>
 #include <Definitions.h>
 
-signed char sign(float A) {
-    return (A < 0) ? -1 : ((A > 0) ? 1 : 0);
-};
-
-float deltaAngle(float current, float target) { // Gets the smallest difference in 2 angle, including its direction
-    if (abs(current - target) < 180) {
-        return target - current;
-    } else {
-        return target - current - 360;
-    }
-};
-
 class Drive {
     public:
-        Drive(int ANGL) :
-            _motorAngle(ANGL) {
-        }
+        Drive(int motorAngle) : _motorAngle(motorAngle) {}
 
         int drive() {
             constrain(_speed, -1, 1);
@@ -28,10 +14,6 @@ class Drive {
             float y = cosf(RAD(_direction));
             float translationCommand = _speed * 255 * (cosf(RAD(_motorAngle)) * x - sinf(RAD(_motorAngle)) * y);
 
-            // analogWrite(_pin0, constrain(command, 0, 255));
-            // analogWrite(_pin1, abs(constrain(command, -255, 0)));
-
-            // JUST FOR DEBUGGING: remember to change function to method
             return translationCommand;
         }
 
@@ -44,7 +26,7 @@ class Drive {
         }
 
     private:
-        const int   _motorAngle;
+        const int _motorAngle;
         float _speed;
         int _direction;
 };
