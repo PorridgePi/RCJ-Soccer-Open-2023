e2bdf5091a2ab5e47287eaa96f70112f2f883dc2
PorridgePi <kunbao2006@126.com>
2023-03-08 16:25:48 +0800

    chore: Speed between -1 and 1, remove rotation, and simplify code Note: Use rotation rate instead of target orientation angle to dissociate drive base rotation from compass

--- lib/private/Drive.h
+++ lib/private/Drive.h
@@ -4,14 +4,9 @@
 #include <Arduino.h>
 #include <Definitions.h>
 
-// float CurrentOrientation = 30; // updated to the IMU readings every frame;
-// Angle of the bot wrt the field float TargetOrientation = 0;
-float currentOrientation = 0;
-float targetOrientation  = 0;
-
 struct Velocity { // Current Position and Orientation; updated by Localisation
-    float speed = 10;
-    float direction = 0;
+    float speed = 0;
+    int direction = 0;
 };
 
 Velocity velocity;
@@ -37,21 +32,18 @@ class Drive {
         }
 
         float drive() {
-            float directionInRadians = RAD(velocity.direction);
-            float x                  = velocity.speed * sinf(directionInRadians);
-            float y                  = velocity.speed * cosf(directionInRadians);
-            float translationCommand = (cosf(RAD(_motorAngle)) * x - sinf(RAD(_motorAngle)) * y);
-            float rotationCommand =
-                sign(deltaAngle(currentOrientation, targetOrientation));
-            float command = translationCommand + rotationCommand;
+            constrain(velocity.speed, -1, 1);
+            float x = sinf(RAD(velocity.direction));
+            float y = cosf(RAD(velocity.direction));
+            float translationCommand = velocity.speed * 255 * (cosf(RAD(_motorAngle)) * x - sinf(RAD(_motorAngle)) * y);
 
             // analogWrite(_pin0, constrain(command, 0, 255));
             // analogWrite(_pin1, abs(constrain(command, -255, 0)));
 
             // JUST FOR DEBUGGING: remember to change function to method
-            return command;
+            return translationCommand;
         }
-    
+
     private:
         const int   _motorAngle;
 };
--- src/test/pico_lib_drive.cpp
+++ src/test/pico_lib_drive.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Drive.h>
 
-Drive A(45);
+Drive A(270); // wheel pointing to 0 degrees
 
 void setup() {
     pinMode(PIN_LED, OUTPUT);
@@ -10,13 +10,18 @@ void setup() {
 
 void loop() {
     // for debugging:
-    if (velocity.direction > 360) {
-        velocity.direction = 0;
-    } else {
-        velocity.direction++;
-        velocity.direction++;
-        velocity.direction++;
-    }
+    // if (velocity.direction > 360) {
+    //     velocity.direction = 0;
+    // } else {
+    //     velocity.direction++;
+    //     velocity.direction++;
+    //     velocity.direction++;
+    // }
+
+    velocity.direction = 0;
+    velocity.speed = 1;
+
+
     Serial.print("Target Orientation: ");
     Serial.print(velocity.direction);
     Serial.print("\tSpeed: ");
