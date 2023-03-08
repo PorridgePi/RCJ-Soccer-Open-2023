86604d8b095e08c1ddababf0491f8f2371e35458
PorridgePi <kunbao2006@126.com>
2023-03-08 15:31:38 +0800

    fix: ci: Errors in code and config that prevent compilation

--- src/test/esp32_led.cpp
+++ src/test/esp32_led.cpp
@@ -7,18 +7,8 @@ void setup() {
     led.begin(18, 10);
     for (int i = 0; i < 10; i++) {
         led.color(i, 100,100,100,100);
-        //led.color(i, 255*(0.5*(1+sin(0.01*millis()))), 10, 10, 10);
     }
     led.show();
-
-}
-void loop() {
-    //led.begin(18, 10);
-    /*for (int i = 0; i < 10; i++) {
-        led.color(i, 10,10,10,10);
-        //led.color(i, 255*(0.5*(1+sin(0.01*millis()))), 10, 10, 10);
-    }
-    led.show();*/
 }
 
 void loop() {
