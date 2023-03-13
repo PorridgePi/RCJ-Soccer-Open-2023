2138b3ec8d7fe97e039e133c104fd40f53ada851
PorridgePi <kunbao2006@126.com>
2023-03-13 18:43:53 +0800

    feat: Add light gate calibration, remove timer, define pin

--- lib/private/Definitions.h
+++ lib/private/Definitions.h
@@ -12,11 +12,12 @@
 #define I2C_READ_ESP32_FREQUENCY 1000 // every x ms
 
 // Thresholds
-#define EMPTY_LIGHT_GATE_THRESHOLD 200
+#define LIGHT_GATE_DIFFERENCE_THRESHOLD 100
 
 // Pins
 #define LIGHT_GATE_PIN A2
 #define PIXY_TX 8
 #define PIXY_RX 15
+#define BOTTOM_PLATE_PIN 1
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define DEBUG true
 #define DEBUG_PRINT_LIGHT_GATE false
 #define DEBUG_PRINT_PIXY false
-#define DEBUG_LOOP_TIME false
+#define DEBUG_LOOP_TIME true
 #define DEBUG_ON_LINE false
 
 Camera Pixy(PIXY_RX, PIXY_TX, 142, 118);
@@ -15,9 +15,19 @@ unsigned long loopStartMicros;
 
 unsigned long long prevI2C = 0;
 int                temtAngle;
+int emptyLightGateThreshold;
+
+int calibrateLightGate() {
+    delay(500);
+    int sum = 0;
+    for (int i = 0; i < 100; i++) {
+        sum += analogRead(LIGHT_GATE_PIN);
+    }
+    return sum / 100;
+}
 
 bool isBallInGate() {
-    if (analogRead(LIGHT_GATE_PIN) < EMPTY_LIGHT_GATE_THRESHOLD) {
+    if (analogRead(LIGHT_GATE_PIN) < emptyLightGateThreshold - LIGHT_GATE_DIFFERENCE_THRESHOLD) {
         return true;
     } else {
         return false;
@@ -25,13 +35,21 @@ bool isBallInGate() {
 }
 
 bool isOnLine() {
-    return digitalRead(1);
+    return digitalRead(BOTTOM_PLATE_PIN);
 }
 
 void setup() {
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, HIGH); // turn on LED to indicate start of setup
+
+    pinMode(BOTTOM_PLATE_PIN, INPUT);
+    pinMode(LIGHT_GATE_PIN, INPUT);
+
     Serial.begin(9600);
     Pixy.begin(19200);
-    pinMode(1, INPUT);
+
+    emptyLightGateThreshold = calibrateLightGate();
+    digitalWrite(PIN_LED, LOW); // turn off LED to indicate end of setup
 }
 
 void loop() {
@@ -43,6 +61,7 @@ void loop() {
         Serial.print(isOnLine()); Serial.print("\t");
     }
     if (DEBUG_PRINT_LIGHT_GATE) {
+        Serial.print(emptyLightGateThreshold); Serial.print("\t");
         Serial.print(isBallInGate()); Serial.print("\t");
         Serial.print(analogRead(LIGHT_GATE_PIN)); Serial.print("\t");
     }
@@ -55,8 +74,6 @@ void loop() {
         Serial.print("\t");
     }
     if (DEBUG_PRINT_LIGHT_GATE || DEBUG_PRINT_PIXY || DEBUG_LOOP_TIME || DEBUG_ON_LINE) Serial.println();
-    
-    if (millis() % 1000 == 0) Serial.println(millis()/1000);
 }
 
 void loop1() {
