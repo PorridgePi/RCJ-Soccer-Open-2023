104a36c8ed254e6bfee47fe6d79c5af92f5bc846
PorridgePi <kunbao2006@126.com>
2023-03-13 18:20:30 +0800

    feat: Update drive test code with correct motor pins and drive squarely

--- src/test/pico_lib_drive.cpp
+++ src/test/pico_lib_drive.cpp
@@ -1,25 +1,30 @@
 #include <Arduino.h>
 #include <Drive.h>
 
-#define MAX_SPEED 1.0
+#define MAX_SPEED 0.5
 
-Motor motorFR(0, 0, MAX_SPEED);
-Motor motorBR(0, 0, MAX_SPEED);
-Motor motorBL(0, 0, MAX_SPEED);
-Motor motorFL(0, 0, MAX_SPEED);
+Motor motorFR(21, 20, MAX_SPEED); // top left JST, top right motor
+Motor motorBR(26, 22, MAX_SPEED); // bottom left JST, bottom right motor
+Motor motorBL(3, 7, MAX_SPEED);  // bottom right JST, bottom left motor
+Motor motorFL(11, 9, MAX_SPEED); // top right JST, top left motor
+
 
 Drive driveBase(motorFR, motorBR, motorBL, motorFL);
 
-int targetAngle, targetSpeed, rotationRate;
+float targetSpeed, rotationRate;
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    targetAngle = 0;
-    targetSpeed = 1;
-    rotationRate = -1;
-
-    driveBase.setDrive(targetSpeed, targetAngle, rotationRate);
+    targetSpeed = 0.2; rotationRate = 0;
+    driveBase.setDrive(targetSpeed, 0, rotationRate);
+    delay(1000);
+    driveBase.setDrive(targetSpeed, 90, rotationRate);
+    delay(1000);
+    driveBase.setDrive(targetSpeed, 180, rotationRate);
+    delay(1000);
+    driveBase.setDrive(targetSpeed, 270, rotationRate);
+    delay(1000);
 }
