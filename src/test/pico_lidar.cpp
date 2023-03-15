06cfc818b6265223331d26013d6f96584a36fa24
PorridgePi <kunbao2006@126.com>
2023-03-15 17:09:57 +0800

    feat: Update LiDAR test code with all sensors and calibration

--- src/test/pico_lidar.cpp
+++ src/test/pico_lidar.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <Lidar.h>
 
-int frontDist;
-Lidar lidarFront(0x10);
+int   frontDist, backDist, rightDist, leftDist;
+Lidar lidarFront(0x12);
+Lidar lidarBack(0x11);
+Lidar lidarRight(0x13);
+Lidar lidarLeft(0x10);
 
 void setup() {
     Wire.setSCL(13);
@@ -12,6 +15,18 @@ void setup() {
 }
 
 void loop() {
-    frontDist = lidarFront.read();
-    Serial.println(frontDist);
+    frontDist = lidarFront.read(); // 2cm from center
+    backDist  = lidarBack.read();  // 2cm from center
+    rightDist = lidarRight.read(); // 3.5cm from center
+    leftDist  = lidarLeft.read();  // 3.5cm from center
+
+    Serial.print("Front: ");
+    Serial.print(frontDist - 5);
+    Serial.print("\tBack: ");
+    Serial.print(backDist + 5);
+    Serial.print("\tRight: ");
+    Serial.print(rightDist + 4);
+    Serial.print("\tLeft: ");
+    Serial.print(leftDist + 4);
+    Serial.println();
 }
