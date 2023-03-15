795867a5702925da27e0b6dffad3a5bea1968f9c
PorridgePi <kunbao2006@126.com>
2023-03-15 17:20:35 +0800

    feat: Update LiDAR test code to use new features

--- src/test/pico_lib_lidar.cpp
+++ src/test/pico_lib_lidar.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 #include <Lidar.h>
 
+int   frontDistRaw, backDistRaw, rightDistRaw, leftDistRaw;
 int   frontDist, backDist, rightDist, leftDist;
-Lidar lidarFront(0x12);
-Lidar lidarBack(0x11);
-Lidar lidarRight(0x13);
-Lidar lidarLeft(0x10);
+Lidar lidarFront(0x12, -5);
+Lidar lidarBack(0x11, +5);
+Lidar lidarRight(0x13, +4);
+Lidar lidarLeft(0x10, +4);
 
 void setup() {
     Wire.setSCL(13);
@@ -15,18 +16,33 @@ void setup() {
 }
 
 void loop() {
+    frontDistRaw = lidarFront.readRaw(); // 2cm from center
+    backDistRaw  = lidarBack.readRaw();  // 2cm from center
+    rightDistRaw = lidarRight.readRaw(); // 3.5cm from center
+    leftDistRaw  = lidarLeft.readRaw();  // 3.5cm from center
+
+    Serial.print("fRaw: ");
+    Serial.print(frontDistRaw);
+    Serial.print("\tbRaw: ");
+    Serial.print(backDistRaw);
+    Serial.print("\trRaw: ");
+    Serial.print(rightDistRaw);
+    Serial.print("\tlRaw: ");
+    Serial.print(leftDistRaw);
+
     frontDist = lidarFront.read(); // 2cm from center
     backDist  = lidarBack.read();  // 2cm from center
     rightDist = lidarRight.read(); // 3.5cm from center
     leftDist  = lidarLeft.read();  // 3.5cm from center
 
-    Serial.print("Front: ");
-    Serial.print(frontDist - 5);
+    Serial.print("\tFront: ");
+    Serial.print(frontDist);
     Serial.print("\tBack: ");
-    Serial.print(backDist + 5);
+    Serial.print(backDist);
     Serial.print("\tRight: ");
-    Serial.print(rightDist + 4);
+    Serial.print(rightDist);
     Serial.print("\tLeft: ");
-    Serial.print(leftDist + 4);
+    Serial.print(leftDist);
+
     Serial.println();
 }
