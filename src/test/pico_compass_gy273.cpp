fa41af8046492ea0d4ecef090bc61ebe2608f3b2
PorridgePi <kunbao2006@126.com>
2023-03-21 13:44:44 +0800

    chore: Rename ori to botHeading and cleanup

--- src/test/pico_compass_gy273.cpp
+++ src/test/pico_compass_gy273.cpp
@@ -17,21 +17,16 @@ Motor motorFL(11, 9, MAX_SPEED);  // top right JST, top left motor
 Drive driveBase(motorFR, motorBR, motorBL, motorFL);
 
 int   frontDist, backDist, rightDist, leftDist;
+int x, y;
 Lidar lidarFront(0x12, -5);
 Lidar lidarRight(0x13, +4);
 Lidar lidarBack(0x11, +5);
 Lidar lidarLeft(0x10, +4);
 
-unsigned long timestamp;
-
-int x, y;
-
 float targetSpeed, rotationRate;
 
+float botHeading
 IMU imu(0x1E);
-
-float targetAngle;
-float moveAngle;
 
 void updatePosition() {
     /*
@@ -47,6 +42,11 @@ void updatePosition() {
     Serial.print("\tLeft: ");
     Serial.print(lidarLeft.read());
 */
+
+    frontDist = lidarFront.read();
+    backDist = lidarBack.read();
+    rightDist = lidarRight.read();
+    leftDist = lidarLeft.read();
 
     x = (lidarLeft.read() - lidarRight.read()) / 2;
     y = (lidarFront.read() - lidarBack.read()) / 2;
@@ -65,15 +65,14 @@ void setup() {
     imu.setCalibration(159, 32, 516, 530, -53);
     imu.init();
     imu.tare();
-    // targetAngle = atan2(mag[0], mag[1]) / PI * 180;
 }
 
 void loop() {
     unsigned long long now = micros();
 
-    float ori = imu.readAngle();
+    botHeading = imu.readAngle();
     // bool isOnLine = digitalRead(1);
-    float rotateAngle = ori <= 180 ? ori : ori - 360; // from -180 to 180
+    float rotateAngle = botHeading <= 180 ? botHeading : botHeading - 360; // from -180 to 180
 
     // Serial.print(rotateAngle);
     // Serial.print("\t");
@@ -92,7 +91,6 @@ void loop() {
     /*
     if (!isOnLine) {
     targetSpeed = 0.3;// rotationRate = 0;
-    //moveAngle =
     driveBase.setDrive(targetSpeed, 0, constrain(-(imu.readAngle())/90,-1,1));
     } else {
         driveBase.setDrive(0.5, 180, 0);
