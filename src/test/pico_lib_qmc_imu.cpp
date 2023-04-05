93c1d96ea0093b1b6b0caa138a53ddbb6460c205
PorridgePi <kunbao2006@126.com>
2023-04-05 17:22:47 +0800

    feat: Add test code for new IMU library

---
+++ src/test/pico_lib_qmc_imu.cpp
@@ -0,0 +1,39 @@
+#include <CommonUtils.h>
+#include <MechaQMC5883.h>
+#include <Wire.h>
+
+MechaQMC5883  qmc(Wire1);
+unsigned long t     = 0;
+float         angle = -1;
+
+void setup() {
+    Wire1.setSCL(3);
+    Wire1.setSDA(2);
+    Wire1.begin();
+    Serial.begin(9600);
+    qmc.init();
+    qmc.tare();
+}
+
+void loop() {
+    t = micros(); // loop time
+
+    // int x, y, z;
+    // byte err = qmc.read(&x, &y, &z);
+    // if (err) {
+    //     qmc.init();
+    //     Serial.print("err: ");
+    //     Serial.print(err);
+    //     Serial.print('\t');
+    //     angle = -1;
+    // } else {
+    //     angle = 360-LIM_ANGLE(DEG(atan2(x, y)));
+    // }
+
+    angle = qmc.readAngle();
+    Serial.print(angle);
+    Serial.print('\t');
+    Serial.print((float) (micros() - t) / 1000);
+    Serial.print("\t");
+    Serial.println();
+}
