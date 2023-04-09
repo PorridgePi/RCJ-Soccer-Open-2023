1635c2b323037d4569b5b6a30ec70c454a458223
PorridgePi <kunbao2006@126.com>
2023-04-09 15:52:02 +0800

    feat: Update IMU calibration code

--- src/test/pico_imu_calibration.cpp
+++ src/test/pico_imu_calibration.cpp
@@ -1,33 +1,39 @@
 #include <Arduino.h>
-#include <IMU.h>
+#include <Definitions.h>
+#include <MechaQMC5883.h>
 #include <Wire.h>
 
-IMU imu(Wire, 0x1E);
+MechaQMC5883 imu(Wire1, -244, -305, 1.05993520658, 56.2635641705);
+// MechaQMC5883 imu(Wire1, 0, 0, 1.07350865912, 66.1081956153);
 
 #include <Drive.h>
 #define MAX_SPEED 0.5
-Motor motorFR(21, 20); // top left JST, top right motor
-Motor motorBR(26, 22); // bottom left JST, bottom right motor
-Motor motorBL(3, 7);   // bottom right JST, bottom left motor
-Motor motorFL(11, 9);  // top right JST, top left motor
+Motor motorFR(PIN_BOT_B_LPWM, PIN_BOT_B_RPWM);
+Motor motorBR(PIN_BOT_A_RPWM, PIN_BOT_A_LPWM);
+Motor motorBL(PIN_TOP_B_RPWM, PIN_TOP_B_LPWM);
+Motor motorFL(PIN_TOP_A_RPWM, PIN_TOP_A_LPWM);
 
 Drive driveBase(motorFR, motorBR, motorBL, motorFL);
 
 void setup() {
     Serial.begin(9600);
 
-    Wire.setSCL(13);
-    Wire.setSDA(12);
-    Wire.begin();
-
+    // I2C for IMU
+    Wire1.setSCL(PIN_WIRE1_SCL);
+    Wire1.setSDA(PIN_WIRE1_SDA);
+    Wire1.setTimeout(1); // set timeout to 1 ms
+    Wire1.begin();
     imu.init();
-
-    imu.setCalibration(159, 32, 516, 530, -53);
     // imu.tare();
 }
 
 void loop() {
-    // driveBase.setDrive(0,0,0.2);
+    if (millis() % 10000 <= 5000) {
+        driveBase.setDrive(0, 0, 0.05, 0);
+    } else {
+        driveBase.setDrive(0, 0, -0.05, 0);
+    }
     imu.printRaw();
-    delay(100);
+    // Serial.println(imu.readAngle());
+    delay(5);
 }
