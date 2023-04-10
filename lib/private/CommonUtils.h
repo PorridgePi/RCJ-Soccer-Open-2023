68a5852eddf074186befdf08928818e7ff1f55af
PorridgePi <kunbao2006@126.com>
2023-04-10 23:54:42 +0800

    feat: Add debug prints and conversion from 360 to 180 macros

--- lib/private/CommonUtils.h
+++ lib/private/CommonUtils.h
@@ -11,5 +11,9 @@
 #define LIM_ANGLE(angle) (angle > 0 ? fmod(angle, 360) : fmod(angle, 360) + 360)
 
 #define DELTA_ANGLE(a, b) ((a - b + 180) % 360 - 180)
+#define ANGLE_360_TO_180(angle) (angle > 180 ? angle - 360 : angle)
+
+#define DPRINT(x) if (DEBUG_PRINT) { Serial.print(#x); Serial.print(": "); Serial.print(x); Serial.print('\t'); }
+#define EPRINT(x) if (DEBUG_PRINT) { Serial.print(x); Serial.print('\t'); }
 
 #endif
