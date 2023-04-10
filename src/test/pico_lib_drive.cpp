c4ae334d41d813bd79a14b0e51654d285380890a
PorridgePi <kunbao2006@126.com>
2023-04-10 23:55:43 +0800

    feat: Only drive straight, increase speed, use correct PID for drive test

--- src/test/pico_lib_drive.cpp
+++ src/test/pico_lib_drive.cpp
@@ -8,7 +8,7 @@
 #ifdef WITH_IMU
 #include <MechaQMC5883.h>
 #include <PID.h>
-PID          pid(0.005, 0, 0.1, 2000);
+PID          pid(0.005, 0, 0.2, 1000);
 MechaQMC5883 imu(Wire1, 0, 0, 0, 0);
 #endif
 
@@ -34,21 +34,26 @@ void setup() {
 void loop() {
     float targetSpeed, rotationRate;
 
-    targetSpeed  = 0.2;
+    targetSpeed  = 0.5;
     rotationRate = 0;
 
     #ifdef WITH_IMU
     float botHeading = imu.readAngle();                                                                                              // from 0 to 360
-    rotationRate     = constrain((LIM_ANGLE(botHeading) <= 180 ? LIM_ANGLE(botHeading) : LIM_ANGLE(botHeading) - 360) / 540, -1, 1); // from -180 to 180
-    rotationRate     = constrain(abs(rotationRate), 0.03, 1) * copysign(1, constrain(pid.compute(0, -(LIM_ANGLE(botHeading) <= 180 ? LIM_ANGLE(botHeading) : LIM_ANGLE(botHeading) - 360)), -1, 1));
+    // rotationRate     = constrain((LIM_ANGLE(botHeading) <= 180 ? LIM_ANGLE(botHeading) : LIM_ANGLE(botHeading) - 360) / 540, -1, 1); // from -180 to 180
+    // rotationRate     = constrain(abs(rotationRate), 0.03, 1) * copysign(1, constrain(pid.compute(0, -(LIM_ANGLE(botHeading) <= 180 ? LIM_ANGLE(botHeading) : LIM_ANGLE(botHeading) - 360)), -1, 1));
+    rotationRate = constrain(pid.compute(0, -(LIM_ANGLE(botHeading) <= 180 ? LIM_ANGLE(botHeading) : LIM_ANGLE(botHeading) - 360)), -1, 1);
+    if (digitalRead(PIN_ORI_RESET) == HIGH) { // reset IMU if button pressed
+        imu.tare();
+    }
     #endif
 
+
     driveBase.setDrive(targetSpeed, 0, rotationRate, 0);
-    delay(1000);
-    driveBase.setDrive(targetSpeed, 90, rotationRate, 0);
-    delay(1000);
-    driveBase.setDrive(targetSpeed, 180, rotationRate, 0);
-    delay(1000);
-    driveBase.setDrive(targetSpeed, 270, rotationRate, 0);
-    delay(1000);
+    // delay(1000);
+    // driveBase.setDrive(targetSpeed, 90, rotationRate, 0);
+    // delay(1000);
+    // driveBase.setDrive(targetSpeed, 180, rotationRate, 0);
+    // delay(1000);
+    // driveBase.setDrive(targetSpeed, 270, rotationRate, 0);
+    // delay(1000);
 }
