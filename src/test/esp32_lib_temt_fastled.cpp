ef1a31cd8b962c9fa8b2f06b176e4b772993facd
PorridgePi <kunbao2006@126.com>
2023-04-11 00:04:19 +0800

    chore: Update test code with updated TEMT lib

--- src/test/esp32_lib_temt_fastled.cpp
+++ src/test/esp32_lib_temt_fastled.cpp
@@ -22,18 +22,18 @@ void setup() {
 // 27         04
 // 14         02
 //    13   15
-
+#define TEMT_DIFF  70
 Temt temts[10] = {
-    Temt(33, 0.4, 1),
-    Temt(32, 1, 0.4),
-    Temt(4, 1, 0),
-    Temt(2, 1, -0.4),
-    Temt(15, 0.4, -1),
-    Temt(13, -0.4, -1),
-    Temt(14, -1, -0.4),
-    Temt(27, -1, 0),
-    Temt(26, -1, 0.4),
-    Temt(25, -0.4, 1),
+    Temt(33, 0.4, 1, TEMT_DIFF),
+    Temt(32, 1, 0.4, TEMT_DIFF),
+    Temt(4, 1, 0, TEMT_DIFF),
+    Temt(2, 1, -0.4, TEMT_DIFF),
+    Temt(15, 0.4, -1, TEMT_DIFF),
+    Temt(13, -0.4, -1, TEMT_DIFF),
+    Temt(14, -1, -0.4, TEMT_DIFF),
+    Temt(27, -1, 0, TEMT_DIFF),
+    Temt(26, -1, 0.4, TEMT_DIFF),
+    Temt(25, -0.4, 1, TEMT_DIFF),
 };
 
 void loop() {
