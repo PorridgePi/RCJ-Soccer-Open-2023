71c8cff8453ad036c55cec9f7272e24c5995f2ce
PorridgePi <kunbao2006@126.com>
2023-04-10 23:56:30 +0800

    feat: Add threshold to TEMT lib

--- lib/private/Temt.h
+++ lib/private/Temt.h
@@ -5,18 +5,34 @@
 
 class Temt {
     public:
-        Temt(int pin, float x, float y) :
-            _pin(pin), X(x), Y(y) { // constructor
+        Temt(int pin, float x, float y, int diff) :
+            _pin(pin), x(x), y(y), _diff(diff) { // constructor
             pinMode(_pin, INPUT);
         };
+
         int read() {
             return analogRead(_pin);
         };
-        const float X;
-        const float Y;
+
+        void setThreshold(int threshold) {
+            _threshold = threshold;
+        };
+
+        bool isOnLine() {
+            return read() > (_threshold + _diff);
+        };
+
+        void printThreshold() {
+            Serial.print(_threshold); Serial.print('\t');
+        };
+
+        const float x;
+        const float y;
 
     private:
         const int _pin;
+        const int _diff;
+        int _threshold;
 };
 
 #endif
