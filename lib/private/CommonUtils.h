28cabcf2a7e86ff45fbbb4b00d6ffab91a8a2e76
AntiSocial-Taco <103398800+AntiSocial-Taco@users.noreply.github.com>
2023-04-14 20:09:31 +0800

    fix: DELTA_ANGLE macro

--- lib/private/CommonUtils.h
+++ lib/private/CommonUtils.h
@@ -10,7 +10,7 @@
 // Limit angle to 0 to 360 degrees
 #define LIM_ANGLE(angle) (angle > 0 ? fmod(angle, 360) : fmod(angle, 360) + 360)
 
-#define DELTA_ANGLE(a, b) ((a - b + 180) % 360 - 180)
+#define DELTA_ANGLE(a, b) (abs(fmod((a - b + 180), 360)) - 180)
 #define ANGLE_360_TO_180(angle) (angle > 180 ? angle - 360 : angle)
 
 #define DPRINT(x) if (DEBUG_PRINT) { Serial.print(#x); Serial.print(": "); Serial.print(x); Serial.print('\t'); }
