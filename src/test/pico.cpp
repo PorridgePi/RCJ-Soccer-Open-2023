1b597faff236867abafdf1ae096fa033dabb0722
PorridgePi <kunbao2006@126.com>
2023-04-15 18:40:26 +0800

    feat: Kicker test code

--- src/test/pico.cpp
+++ src/test/pico.cpp
@@ -1,17 +1,12 @@
 #include <Arduino.h>
+#include <Kicker.h>
+#include <Definitions.h>
 
-void setup() {
-    Serial.begin(115200);
-    Serial2.setRX(5);
-    Serial2.setTX(4);
-    Serial2.begin(4800);
-}
+Kicker kicker(PIN_RELAY);
+
+void setup() {}
 
 void loop() {
-    static uint8_t writeBuffer[6] = {174, 193, 32, 2, 255, 255};
-    if (Serial2.available() == 0) Serial2.write(writeBuffer,6);
-    if (Serial2.available()) {
-      Serial.print(Serial2.read());
-      Serial.print(' ');
-    }
+    kicker.kick();
+    delay(10000);
 }
